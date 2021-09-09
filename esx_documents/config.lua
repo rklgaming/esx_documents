aaa56bc56184cd13ad59c3d983720886f0494d88
RKLGaming <90053019+rklgaming@users.noreply.github.com>
2021-09-10 07:36:57 +0800

    Update config.lua

--- esx_documents/config.lua
+++ esx_documents/config.lua
@@ -1,6 +1,7 @@
 Config = {}
 
 Config.MenuKey 			= 182 -- "L" button, change to whatever you want
+Config.UseKey           = true -- this is menuKey
 Config.UseCustomFonts 	= false -- Leave this as is if you don't know how to or haven't loaded custom fonts.
 Config.CustomFontFile 	= "greek" -- change only if you turn custom fonts on.
 Config.CustomFontId 	= "OpenSans" -- change only if you turn custom fonts ok.
